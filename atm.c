ed2d5c0ba0b992b2ff9270fb05bc6cfb254f9e66
VMNik <106076576+VMNik@users.noreply.github.com>
2022-05-27 11:55:13 +0530

    Add files via upload

---
+++ atm.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+unsigned long amount = 1000, deposit, withdraw;
+int choice, pin, k;
+char transaction = 'y';
+void main()
+{
+  while (pin != 1520)
+  {
+    printf("ENTER YOUR SECRET PIN NUMBER:");
+    scanf("%d", & pin);
+    if (pin != 1520)
+      printf("PLEASE ENTER VALID PASSWORD! \a\n");
+  }
+  do
+  {
+  	system("cls");
+    printf("\n**Welcome to XYZ Bank ATM Service*\n");
+    printf("1. Check Balance\n");
+    printf("2. Withdraw Cash\n");
+    printf("3. Deposit Cash\n");
+    printf("4. Quit\n");
+    printf("******\n\n");
+    printf("Enter your choice: ");
+    scanf("%d", & choice);
+    switch (choice)
+    {
+    case 1:
+      system("cls");	
+      printf("\n YOUR BALANCE IS TK : %lu ", amount);
+      break;
+    case 2:
+    	system("cls");
+      printf("\n ENTER THE AMOUNT TO WITHDRAW: ");
+      scanf("%lu", & withdraw);
+      if (withdraw % 100 != 0)
+      {
+        printf("\n PLEASE ENTER THE AMOUNT IN MULTIPLES OF 100");
+      } else if (withdraw > (amount - 500))
+      {
+        printf("\n INSUFFICENT BALANCE");
+      } else
+      {
+        amount = amount - withdraw;
+        printf("\n\n PLEASE COLLECT CASH");
+        printf("\n YOUR CURRENT BALANCE IS %lu", amount);
+      }
+      break;
+    case 3:
+    	system("cls");
+      printf("\n ENTER THE AMOUNT TO DEPOSIT");
+      scanf("%lu", & deposit);
+      amount = amount + deposit;
+      printf("YOUR BALANCE IS %lu", amount);
+      break;
+    case 4:
+    	system("cls");
+      printf("\n THANK YOU FOR USING XYZ Bank ATM");
+      break;
+    default:
+    	system("cls");
+      printf("\n INVALID CHOICE");
+    }
+    printf("\n\n\n DO U WISH TO HAVE ANOTHER TRANSCATION?(y/n): \n");
+    fflush(stdin);
+    scanf("%c", & transaction);
+    if (transaction == 'n' || transaction == 'N')
+      k = 1;
+  } while (!k);
+  system("cls");
+  printf("\n\n THANKS FOR USING XYZ Bank ATM SERVICE");
+}
